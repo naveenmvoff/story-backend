b741790d5f5bfd5fa116f78280d3d04084be6954
naveenmvoff <naveenmvoff@gmai.com>
2025-03-31 12:01:32 +0530

    Inital Setup4

--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,5 @@ from app import app
 import os
 
 if __name__ == "__main__":
-    # Bind to 0.0.0.0 to allow external connections
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5001)))
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port)
